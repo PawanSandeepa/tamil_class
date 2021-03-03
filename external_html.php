6c4708ed4a45d778ce0512ac985da0b5bb6a3f62
pawansandeepa19951224 <54311556+pawansandeepa19951224@users.noreply.github.com>
2021-03-04 01:44:09 +0530

    Add files via upload

---
+++ external_html.php
@@ -0,0 +1,48 @@
+<?php
+  function navi(){
+    echo '<nav class="navbar navbar-dark fixed-top navbar-expand-sm bg-dark">
+      <a href="#" class="navbar-brand mx-3">Our Tamil Class</a>
+      <button class="navbar-toggler" data-toggle="collapse" data-target="#nav1"><span class="navbar-toggler-icon"></span></button>
+      <div class="collapse navbar-collapse justify-content-center" id="nav1">
+        <ul class="navbar-nav mx-3">
+          <li class="nav-item"><a href="#" class="nav-link">Home</a></li>
+          <li class="nav-item"><a href="#" class="nav-link">Abot</a></li>
+          <li class="nav-item"><a href="#" class="nav-link">Contact</a></li>
+          <li class="nav-item"><a href="#" class="nav-link">Feedback</a></li>
+        </ul>
+      </div>
+    </nav>';
+  }
+
+  function footer(){
+    echo '
+      <footer id="footer">
+        <div class="container-flid bg-secondary">
+          <div class="row d-flex">
+            <div class="col-lg-1 ml-3">
+              <img src="site/img/avigon_logo.png" alt="avigon" style="width: 100px;">
+            </div>
+            <div class="col-lg-6 text-lg-left mt-4">
+              <div class="copyright" style="color:white">
+                &copy; Copyright <strong>Avigon</strong>. All Rights Reserved
+              </div>
+              <div class="credits" style="color:white">
+                Designed by <a href="https://avigon.com/">Avigon</a>
+              </div>
+            </div>
+            <div class="col-lg-5 mt-4">
+              <nav class="footer-links text-lg-right text-center">
+                <a href="#" class="fa fa-facebook btn btn-lg btn-outline-primary"></a>
+                <a href="#" class="fa fa-twitter btn btn-lg btn-outline-info"></a>
+                <a href="#" class="fa fa-google btn btn-lg btn-outline-danger"></a>
+                <a href="#" class="fa fa-linkedin btn btn-lg btn-outline-primary"></a>
+                <a href="#" class="fa fa-youtube btn btn-lg btn-outline-danger"></a>
+                <a href="#" class="fa fa-instagram btn btn-lg btn-outline-danger"></a>
+              </nav>
+            </div>
+          </div>
+        </div>
+      </footer>';
+  }
+
+?>
---
+++ index.php
@@ -0,0 +1,94 @@
+<?php require_once('external_html.php'); ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+
+  <title>Tamil class</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+  <link href="site/img/favicon.png" rel="icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+
+  <link rel="stylesheet" href="css/bootstrap">
+
+  
+</head>
+
+<body>
+  <?php navi(); ?>
+  <div class="jumbotron">
+    <div class="container-fluid py-5 px-5 bg-light">
+      <div class="row">
+        <div class="col-lg-6 pt-5 pt-lg-0 order-2 order-lg-1 d-flex flex-column justify-content-center">
+          <h1 class="display-3">Rasika Sanjeewa</h1>
+          <h4 class="display-6">lankawatama tamil ugannanna hadanne...</h4>
+          <div class="mt-3">
+            <hr>
+            <a href="login.php" class="btn btn-outline-success btn-lg">Sign In</a>
+            <a href="register.php" class="btn btn-outline-primary btn-lg">Sign Up</a>
+          </div>
+        </div>
+        <div class="col-lg-6 order-1 order-lg-2 hero-img">
+          <img src="site/img/main.png" class="img-fluid mx-auto d-block" alt="">
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="card-group">
+    <div class="clo-12 col-sm-6 col-md-4 col-lg-4 col-xl-3">
+      <div class="card mx-3 my-3">
+        <img src="site/img/clock.png" class="card-img-top" alt="time_table">
+        <div class="card-body">
+          <h5 class="card-title">Time Table</h5>
+          <p class="card-text">Click here for view our time table</p>
+        </div>
+      </div>
+    </div>
+    <div class="clo-12 col-sm-6 col-md-4 col-lg-4 col-xl-3">
+      <div class="card mx-3 my-3">
+        <img src="site/img/pencil.png" class="card-img-top" alt="time_table">
+        <div class="card-body">
+          <h5 class="card-title">Vocabulary</h5>
+          <p class="card-text">Train with Tamil Vocabulary</p>
+        </div>
+      </div>
+    </div>
+    <div class="clo-12 col-sm-6 col-md-4 col-lg-4 col-xl-3">
+      <div class="card mx-3 my-3">
+        <img src="site/img/note.png" class="card-img-top" alt="time_table">
+        <div class="card-body">
+          <h5 class="card-title">Free Note</h5>
+          <p class="card-text">Click here for get free notes</p>
+        </div>
+      </div>
+    </div>
+    <div class="clo-12 col-sm-6 col-md-4 col-lg-4 col-xl-3">
+      <div class="card mx-3 my-3">
+        <img src="site/img/phrases.png" class="card-img-top" alt="time_table">
+        <div class="card-body">
+          <h5 class="card-title">Phrases</h5>
+          <p class="card-text">Click here for View phrases</p>
+        </div>
+      </div>
+    </div>
+  </div>
+<?php footer(); ?>
+
+<script src="js/bootstrap"></script>
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+</body>
+
+</html>
